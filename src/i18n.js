6cc146fd58fbd9be72b45430729a12565fd7e45b
Taner Acar <151092957+taneracar@users.noreply.github.com>
2025-01-27 02:53:41 +0300

    fixing the language bug

--- src/i18n.js
+++ src/i18n.js
@@ -2,19 +2,22 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
-import en from "../src/locales/en/translation.json"
-import es from "../src/locales/es/translation.json"
-const resources = {
-  en: { translation: en },
-  es: { translation: es },
-};
+import enTranslation from "./locales/en/translation.json"; // Importing JSON using import
+import esTranslation from "./locales/es/translation.json";
 
 i18n
   .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      es: {
+        translation: esTranslation,
+      },
+    },
     supportedLngs: ["en", "es"],
     fallbackLng: "en",
     debug: false,
@@ -34,7 +37,7 @@ i18n
       caches: ["cookie"],
     },
     interpolation: {
-      escapeValue: false, 
+      escapeValue: false,
     },
   });
 
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-import { loadEnv, defineConfig } from "vite";
-import dotenvExpand from "dotenv-expand";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  if (mode === "development") {
-    const env = loadEnv(mode, process.cwd(), "");
-    dotenvExpand.expand({ parsed: env });
-  }
-
-  return {
-    plugins: [react(), tailwindcss()],
-  };
-});
-
-/* export default defineConfig({
+export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
- */
