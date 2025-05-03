95fb9bf48065eabac05e4970529c6cc41a35f221
taner.acar <taneracar1@icloud.com>
2025-05-03 23:17:14 +0300

    change order of the hero images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,7 @@ const Hero = () => {
   return (
     <section
       {...swipeHandlers}
-      className="relative w-full h-[90vh] bg-gray-900 text-white overflow-hidden"
+      className="relative w-full max-sm:h-[80vh] h-[90vh] bg-gray-900 text-white overflow-hidden"
     >
       <div className="absolute inset-0 z-10">
         <AnimatePresence>
