22cdbce641b9ff326e08244eb2a616bc118f1bf3
taner.acar <taneracar1@icloud.com>
2025-06-04 11:52:53 +0300

    add es translations

--- src/components/HomeTestimonials.jsx
+++ src/components/HomeTestimonials.jsx
@@ -5,54 +5,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
 import { useTranslation } from "react-i18next";
-
-const testimonials = [
-  {
-    quote:
-      "The only mechanic I will go to or send anyone!! Not too many mechanics are honest. This is the only place that I will recommend to anyone.",
-    name: "Jody M.",
-    title: "Downingtown, PA",
-    vehicle: "Toyota Tacoma",
-    rating: 5,
-    date: "01/24/2025",
-  },
-  {
-    quote:
-      "I feel comfortable taking my car to Pelle's for inspection knowing he and his team won't try to perform unnecessary repairs (e.g. cabin filter, air filter, wiper blades, coolant flush) like dealerships and the chain mechanics are trained to do.",
-    name: "John N.",
-    title: "Malvern, PA",
-    vehicle: "Dodge Pickup",
-    rating: 5,
-    date: "12/09/2024",
-  },
-  {
-    quote:
-      "Charlie Pelle and his crew always do an incredible job with repairs or simple advice. They don’t upsell you in something you don’t need. Great, honest work!",
-    name: "Rick S.",
-    title: "Exton, PA",
-    vehicle: "Ford F-150",
-    rating: 5,
-    date: "12/22/2024",
-  },
-  {
-    quote:
-      "What a great shop! My Dodge Ram passenger axle had gone bad. They fit me into their schedule, allowed me to supply the parts, and repaired it at half the cost of the dealership.",
-    name: "Tony M.",
-    title: "",
-    vehicle: "Dodge Pickup",
-    rating: 5,
-    date: "12/18/2024",
-  },
-  {
-    quote:
-      "Outstanding service and repair! Always trust Charlie and mechanics with all my vehicles and service. Very reliable and reasonable prices for great service! Highly recommend them for all your vehicle needs!",
-    name: "Barry C.",
-    title: "Malvern, PA",
-    vehicle: "Ford F-150",
-    rating: 5,
-    date: "10/29/2024",
-  },
-];
+import { testimonials } from "../constants/testimonials";
 
 const HomeTestimonials = () => {
   const { t } = useTranslation();
@@ -64,12 +17,12 @@ const HomeTestimonials = () => {
             <div className="flex justify-center items-center gap-2">
               <BeatSVG width={50} height={50} />
               <h3 className="text-[#1E5DBC] text-2xl md:text-3xl font-bold ">
-                TESTIMONIALS
+                {t("testimonialsPage.testimonials")}
               </h3>
             </div>
             <div className="mt-2">
               <h2 className="text-[#1e2939] text-3xl md:text-4xl font-bold uppercase">
-                What our clients say
+                {t("testimonialsPage.titleHome")}
               </h2>
             </div>
           </div>
@@ -97,14 +50,16 @@ const HomeTestimonials = () => {
               }}
               modules={[Pagination]}
             >
-              {testimonials.map((t, index) => (
+              {testimonials.map((testimonial, index) => (
                 <SwiperSlide key={index}>
                   <div className="bg-white w-full p-8 rounded-lg shadow-md text-left flex flex-col justify-between h-[350px]">
                     <div>
                       <div className="mb-10">
                         <VectorSVG />
                       </div>
-                      <p className="text-gray-700 mb-6">{t.quote}</p>
+                      <p className="text-gray-700 mb-6">
+                        {t(`testimonials.reviews.${testimonial.reviewKey}`)}
+                      </p>
                     </div>
                     <div className="flex items-center justify-between">
                       <div className="flex items-center gap-4">
@@ -117,17 +72,28 @@ const HomeTestimonials = () => {
                         /> */}
                         <div>
                           <p className="font-semibold text-[#1E5DBC]">
-                            {t.name}
+                            {testimonial.name}
                           </p>
-                          <p className="text-gray-500 text-sm">{t.title}</p>
+                          <p className="text-gray-500 text-sm">
+                            {" "}
+                            {testimonial.locationKey && (
+                              <p>
+                                {t(
+                                  `testimonials.locations.${testimonial.locationKey}`
+                                )}
+                              </p>
+                            )}
+                          </p>
                         </div>
                       </div>
                       <div className="text-right text-sm">
-                        <p className="text-black font-semibold">{t.vehicle}</p>
-                        <p className="text-yellow-500">
-                          {"★".repeat(t.rating)}
+                        <p className="text-black font-semibold">
+                          {t(`testimonials.vehicles.${testimonial.vehicleKey}`)}
                         </p>
-                        <p className="text-gray-400">{t.date}</p>
+                        <p className="text-yellow-500">
+                          {"★".repeat(testimonial.rating)}
+                        </p>
+                        <p className="text-gray-400">{testimonial.date}</p>
                       </div>
                     </div>
                   </div>
